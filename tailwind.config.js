1327568b3ca3a6a8708baae6ddcc6cb64bad1511
Samiskina <mureraksamantha@gmail.com>
2022-10-17 16:00:56 +0200

    Fix: fixed small screen break-point

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
-      screens:{
-        sm: {max: '800px'},
-        md: {max: '1024px'}
+      screens: {
+        sm: { max: '800px' },
+        md: { min: '1024px' }
       }
     },
   },
